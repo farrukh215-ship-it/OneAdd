ceac277ad4c9cffb7d86fd7be2aa866863699b44
Farrukh <farrukhforwork@gmail.com>
2026-03-05 07:05:04 +0500

    Fix SearchModule JwtService injection to stop API restart

--- services/api/src/search/search.module.ts
+++ services/api/src/search/search.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 import { SearchController } from "./search.controller";
 import { SearchService } from "./search.service";
 
 @Module({
+  imports: [JwtModule],
   controllers: [SearchController],
   providers: [SearchService],
   exports: [SearchService]
