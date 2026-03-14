9eabe3e94c74f588cf84ef855ae3fcadc94b0a4f
Farrukh <farrukhforwork@gmail.com>
2026-03-14 14:21:38 +0500

    Fix Metro module resolution for Expo dependencies in EAS builds

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -5,6 +5,8 @@ const config = getDefaultConfig(__dirname);
 const nativewindBabelPath = require.resolve('nativewind/babel');
 const nativewindDir = path.dirname(nativewindBabelPath);
 const mobileNodeModules = path.resolve(__dirname, 'node_modules');
+const workspaceRoot = path.resolve(__dirname, '../..');
+const workspaceNodeModules = path.join(workspaceRoot, 'node_modules');
 
 config.resolver.extraNodeModules = {
   ...(config.resolver.extraNodeModules || {}),
@@ -12,7 +14,10 @@ config.resolver.extraNodeModules = {
   'react-native': path.join(mobileNodeModules, 'react-native'),
   'react-native-css-interop': path.resolve(nativewindDir, '../react-native-css-interop'),
 };
-config.resolver.nodeModulesPaths = [mobileNodeModules];
-config.resolver.disableHierarchicalLookup = true;
+config.watchFolders = Array.from(new Set([...(config.watchFolders || []), workspaceRoot]));
+config.resolver.nodeModulesPaths = Array.from(
+  new Set([...(config.resolver.nodeModulesPaths || []), mobileNodeModules, workspaceNodeModules]),
+);
+config.resolver.disableHierarchicalLookup = false;
 
 module.exports = config;
