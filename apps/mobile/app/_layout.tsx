87d8d3479caa09a50cd05999b6f669b665f70bc7
Farrukh <farrukhforwork@gmail.com>
2026-03-16 13:18:33 +0500

    fix(mobile): import nativewind global styles

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler';
+import '../global.css';
 
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
